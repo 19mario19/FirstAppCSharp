c79f2ada917e2370965ff8b4dfca0d0753cd5960
Mario M <107927311+19mario19@users.noreply.github.com>
2024-04-24 08:01:40 +0100

    Add project files.

---
+++ FirstAppCSharp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FirstAppCSharp
+{
+    class Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        private string? _name = "SECRET";
+
+        public string getPrivateName()
+        {
+            if(Name != null)
+            {
+                return _name;
+            }
+            return null;
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"My name is {Name} and I am {Age} years old!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Alina";
+            
+            person.Age = 12;
+
+            Console.WriteLine(person.Name);
+            int number = 5;
+            if(number == 3)
+            {
+
+            Console.WriteLine("Hello, World!");
+            } else
+            {
+                Console.WriteLine($"And the private name is {person.getPrivateName()}");
+                Console.WriteLine("Is different than three!");
+                person.Greeting();
+            }
+        }
+    }
+}
