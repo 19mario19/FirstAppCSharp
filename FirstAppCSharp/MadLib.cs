e8d4c8651cc94e4a46e48f2e82906e17f6eba0d4
Mario M <107927311+19mario19@users.noreply.github.com>
2024-04-26 08:40:53 +0100

    To do class

---
+++ FirstAppCSharp/MadLib.cs
@@ -0,0 +1,98 @@
+﻿namespace FirstAppCSharp
+{
+    internal partial class Program
+    {
+        class MadLib
+        {
+            public string[] Adjectives = new string[9] { "amazing", "exciting", "adventurous", "thrilling", "daring", "wonderful", "fantastic", "incredible", "marvelous" };
+            public string[] Nouns = new string[8] { "hero", "explorer", "adventurer", "traveler", "journey", "quest", "voyage", "expedition" };
+            public string[] Verbs = new string[3] { "embark", "venture", "discover" };
+            public string[] PluralNouns = new string[2] { "treasures", "wonders" };
+            public string? Exclamation { get; set; } = "Awesome!";
+            public string? Name { get; set; } = "Brave Explorer";
+
+            public void AddData()
+            {
+                Console.Write("Write your name: ");
+                string YourName = Console.ReadLine();
+                if (string.IsNullOrEmpty(YourName))
+                {
+                    Name = YourName;
+                }
+
+                Console.WriteLine("Adding adjectives:");
+                for (int i = 0; i < Adjectives.Length; i++)
+                {
+                    Console.Write($"Adjective {i} adding: ");
+                    string value = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        Adjectives[i] = value;
+                    }
+                }
+                Console.WriteLine("Adding nouns:");
+                for (int i = 0; i < Nouns.Length; i++)
+                {
+                    Console.Write($"Noun {i} adding: ");
+                    string value = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        Nouns[i] = value;
+                    }
+                }
+                Console.WriteLine("Adding verbs:");
+                for (int i = 0; i < Verbs.Length; i++)
+                {
+                    Console.Write($"Verb {i} adding: ");
+                    string value = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        Verbs[i] = value;
+                    }
+                }
+                Console.WriteLine("Adding plural nouns:");
+                for (int i = 0; i < PluralNouns.Length; i++)
+                {
+                    Console.Write($"Plural noun {i} adding: ");
+                    string value = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(value))
+                    {
+                        PluralNouns[i] = value;
+                    }
+                }
+                Console.WriteLine("Adding exclamation:");
+                string value2 = Console.ReadLine();
+                if (!string.IsNullOrEmpty(value2))
+                {
+                    Exclamation = value2;
+                }
+
+
+            }
+
+            public void FormatText()
+            {
+                Console.WriteLine("The Magical Adventure");
+                Console.WriteLine($@"
+\----
+
+One {Adjectives[0]} day, a {Nouns[0]} named {Name} decided to go on a {Adjectives[1]} adventure.
+They packed their {Nouns[1]} andset off into the {Adjectives[2]} forest.
+As they were {Verbs[0]} through the trees, they stumbled upon a {Adjectives[3]} {Nouns[2]}.
+It was a magical {Nouns[3]} that could grant {PluralNouns[0]}!
+""{Exclamation},"" said {Name}. ""I wish for {PluralNouns[1]}!""
+Suddenly, {PluralNouns[0]} appeared out of nowhere and began {Verbs[1]} around them. 
+{Name} continued their journey and came across a {Adjectives[4]} {Nouns[4]}.
+Inside, they found a {Adjectives[5]} {Nouns[5]} that could {Verbs[2]}.
+After {Verbs[0]} with the {Nouns[5]}, {Name} felt {Adjectives[6]} and decided to head back home.
+On their way back, they encountered a {Adjectives[7]} {Nouns[6]} that could {Verbs[2]}.
+{Name} {Verbs[2]} it and continued their journey.
+When they finally arrived home, {Name} realized that their {Adjectives[6]} adventure had been {Adjectives[7]} and {Adjectives[8]}.
+
+\-----
+");
+            }
+        }
+    }
+}
+
--- FirstAppCSharp/Program.cs
+++ FirstAppCSharp/Program.cs
@@ -1,118 +1,90 @@
 ﻿namespace FirstAppCSharp
 {
-    internal class Program
+    internal partial class Program
     {
-
-        class MadLib
+        class Task
         {
-            public string[] Adjectives = new string[9] { "amazing", "exciting", "adventurous", "thrilling", "daring", "wonderful", "fantastic", "incredible", "marvelous" };
-            public string[] Nouns = new string[8] { "hero", "explorer", "adventurer", "traveler", "journey", "quest", "voyage", "expedition" };
-            public string[] Verbs = new string[3] { "embark", "venture", "discover" };
-            public string[] PluralNouns = new string[2] { "treasures", "wonders" };
-            public string? Exclamation { get; set; } = "Awesome!";
-            public string? Name { get; set; } = "Brave Explorer";
+            public int Id = 0;
+            public string? name { get; set; }
+            public bool? done = false;
+            private static int Count = 0;
 
-            public void AddData()
+            public Task(string? _name)
             {
-                Console.Write("Write your name: ");
-                string YourName = Console.ReadLine();
-                if (string.IsNullOrEmpty(YourName))
-                {
-                    Name = YourName;
-                }
-
-                Console.WriteLine("Adding adjectives:");
-                for (int i = 0; i < Adjectives.Length; i++)
-                {
-                    Console.Write($"Adjective {i} adding: ");
-                    string value = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(value))
-                    {
-                        Adjectives[i] = value;
-                    }
-                }
-                Console.WriteLine("Adding nouns:");
-                for (int i = 0; i < Nouns.Length; i++)
-                {
-                    Console.Write($"Noun {i} adding: ");
-                    string value = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(value))
-                    {
-                        Nouns[i] = value;
-                    }
-                }
-                Console.WriteLine("Adding verbs:");
-                for (int i = 0; i < Verbs.Length; i++)
-                {
-                    Console.Write($"Verb {i} adding: ");
-                    string value = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(value))
-                    {
-                        Verbs[i] = value;
-                    }
-                }
-                Console.WriteLine("Adding plural nouns:");
-                for (int i = 0; i < PluralNouns.Length; i++)
-                {
-                    Console.Write($"Plural noun {i} adding: ");
-                    string value = Console.ReadLine();
-                    if (!string.IsNullOrEmpty(value))
-                    {
-                        PluralNouns[i] = value;
-                    }
-                }
-                Console.WriteLine("Adding exclamation:");
-                string value2 = Console.ReadLine();
-                if (!string.IsNullOrEmpty(value2))
-                {
-                    Exclamation = value2;
-                }
-
-
+                name = _name;
+                Id++;
+                Count++;
             }
 
-            public void FormatText()
+            public void Print()
             {
-                Console.WriteLine("The Magical Adventure");
-                Console.WriteLine($@"
-\----
-
-One {Adjectives[0]} day, a {Nouns[0]} named {Name} decided to go on a {Adjectives[1]} adventure.
-They packed their {Nouns[1]} andset off into the {Adjectives[2]} forest.
-As they were {Verbs[0]} through the trees, they stumbled upon a {Adjectives[3]} {Nouns[2]}.
-It was a magical {Nouns[3]} that could grant {PluralNouns[0]}!
-""{Exclamation},"" said {Name}. ""I wish for {PluralNouns[1]}!""
-Suddenly, {PluralNouns[0]} appeared out of nowhere and began {Verbs[1]} around them. 
-{Name} continued their journey and came across a {Adjectives[4]} {Nouns[4]}.
-Inside, they found a {Adjectives[5]} {Nouns[5]} that could {Verbs[2]}.
-After {Verbs[0]} with the {Nouns[5]}, {Name} felt {Adjectives[6]} and decided to head back home.
-On their way back, they encountered a {Adjectives[7]} {Nouns[6]} that could {Verbs[2]}.
-{Name} {Verbs[2]} it and continued their journey.
-When they finally arrived home, {Name} realized that their {Adjectives[6]} adventure had been {Adjectives[7]} and {Adjectives[8]}.
-
-\-----
-");
+                Console.WriteLine($"Task name: {name} is done: {done}");
             }
+
+            public int HowManyTasks()
+            {
+                Console.WriteLine($"{Count} tasks are created!");
+                return Count;
+            }
+
+            public void TaskSwitch()
+            {
+                done = !done;
+            }
+
+            public void TaskDone()
+            {
+                done = true;
+            }
+            public void TaskNotDone()
+            {
+                done = false;
+            }
+
         }
+        class ToDo
+        {
+            List<Task> list = new List<Task>();
+            public int HowManyLists = 0;
+            public ToDo()
+            {
+                HowManyLists++;
+            }
 
+            public void AddTask(string name)
+            {
+                list.Add(new Task(name));
+            }
+            public void RemoveTask(int Id)
+            {
+                // Find the task with the specified Id
+                Task? taskToRemove = list.Find(t => t.Id == Id);
+
+                if (taskToRemove != null)
+                {
+                    list.Remove(taskToRemove);
+                }
+                else
+                {
+                    Console.WriteLine($"Task with Id {Id} not found.");
+                }
+            }
+
+        }
 
 
         internal static void Main(string[] args)
         {
 
+            Task task = new Task("fasdf", false);
+            task.TaskSwitch();
+            task.Print();
+            task.TaskSwitch();
+            task.Print();
 
-            MadLib matlib = new MadLib();
-            Console.WriteLine("Add your data?");
-            Console.WriteLine("Y or Yes for introducing own data, anything else for defaults");
-            string Question = Console.ReadLine();
-            if (!string.IsNullOrEmpty(Question))
-            {
-                if (Question.ToLower() == "yes" || Question.ToLower() == "y")
-                {
-                    matlib.AddData();
-                }
-            }
-            matlib.FormatText();
+            // Create a to do list 
+
+
 
         }
     }
